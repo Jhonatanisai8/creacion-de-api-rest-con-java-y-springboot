d4e3d7146bab36b26fa03c1759e9ff80f5384a48
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-02 15:45:55 -0500

    metodo para obtener todos los usuarios

--- src/main/java/com/jhonatan/controllers/UserController.java
+++ src/main/java/com/jhonatan/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.jhonatan.controllers;
 
+import com.jhonatan.models.User;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @RequestMapping(value = "/holaMundo", method = RequestMethod.GET)
-    String holaMundo() {
-        return "hola Mundo desde Spring";
+    //trae todos los usuarios
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    List<User> getAll() {
+        List<User> listaUsuarios = new ArrayList<>();
+        User user = new User(1, "David", "Palacios", "David@gmail.com", "234234", new Date());
+        listaUsuarios.add(user);
+        return listaUsuarios;
     }
+
 }
