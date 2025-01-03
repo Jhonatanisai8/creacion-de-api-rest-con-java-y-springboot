aa1aea344daed46dd5437b7642c286b366070084
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-02 21:20:34 -0500

    mapeando la clase user para que se convierta en una entidad en la base de datos

--- src/main/java/com/jhonatan/models/User.java
+++ src/main/java/com/jhonatan/models/User.java
@@ -1,14 +1,35 @@
 package com.jhonatan.models;
 
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity // indicamos que es un entidad 
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
     private long id;
-    private String nombre,
-            apellido,
-            email,
-            telefono;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "fechaNacimiento")
     private Date fechaNacimiento;
 
     public User(long id, String nombre, String apellido, String email, String telefono, Date fechaNacimiento) {
