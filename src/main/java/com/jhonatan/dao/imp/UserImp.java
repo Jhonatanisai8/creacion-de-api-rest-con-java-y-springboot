90f95de431a30c180a122f4563ff9610cc8674c7
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-02 21:50:41 -0500

    clase USER IMPLE: agregando la anotacion Transactional a cada uno de los metodos

--- src/main/java/com/jhonatan/dao/imp/UserImp.java
+++ src/main/java/com/jhonatan/dao/imp/UserImp.java
@@ -5,6 +5,7 @@ import com.jhonatan.models.User;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 
 public class UserImp
         implements UserDao {
@@ -12,28 +13,33 @@ public class UserImp
     @PersistenceContext
     EntityManager entityManager;
 
+    @Transactional
     @Override
     public List<User> getAll() {
         String query = "FROM User as u "; // query en formato que usa hibernate
         return entityManager.createNamedQuery(query).getResultList();
     }
 
+    @Transactional
     @Override
     public User get(long id) {
         return entityManager.find(User.class, id);//mapea el usuario
     }
 
+    @Transactional
     @Override
     public User register(User user) {
         entityManager.merge(user);
         return user;
     }
 
+    @Transactional
     @Override
     public User update(User user) {
         return entityManager.merge(user);
     }
 
+    @Transactional
     @Override
     public void delete(long id) {
         User userBuscado = get(id);
